11235b92a86636b9fcf87ac3794651183801f424
khil02 <120340087+khil02@users.noreply.github.com>
2023-04-28 22:09:03 -0700

    Used jQuery to make

---
+++ js/script.js
@@ -0,0 +1,35 @@
+function newItem(){
+
+    let list = $('#list');
+
+    let li = $('<li></li>');
+    let inputValue = $('#input').val();
+    li.append(inputValue);
+
+     if (inputValue.length === " "){
+            alert("You must write something");
+        } else {
+            list.append(li);
+        }
+    
+function crossOut(){
+    li.toggleClass("strike");
+}
+
+    li.on("dblclick", crossOut);
+
+
+    let crossOutButton = $('<crossOutButton></crossOutButton>');
+    crossOutButton.append(document.createTextNode('X'));
+        li.append(crossOutButton);
+    
+   
+    crossOutButton.on("click", deleteListItem);
+
+    function deleteListItem(){
+        li.addClass("delete");
+    }
+
+    $('#list').sortable();
+
+};
